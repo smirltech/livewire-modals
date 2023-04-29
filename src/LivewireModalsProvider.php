cbe4cd88a238acb13bad7f6d2114a006270f3542
Marien Mupenda <marien.mupenda@gmail.com>
2023-04-29 07:53:07 +0200

    add delete model

--- src/LivewireModalsProvider.php
+++ src/LivewireModalsProvider.php
@@ -3,7 +3,10 @@
 namespace SmirlTech\LivewireModals;
 
 use Livewire\Livewire;
+use SmirlTech\LaravelMedia\Livewire\AvatarComponent;
+use SmirlTech\LaravelMedia\Livewire\GalleryComponent;
 use SmirlTech\LivewireModals\Components\Modals;
+use SmirlTech\LivewireModals\Livewire\DeleteModel;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -19,5 +22,6 @@ class LivewireModalsProvider extends PackageServiceProvider
     public function packageBooted(): void
     {
         Livewire::component('modals', Modals::class);
+        Livewire::component('modals::delete-model', DeleteModel::class);
     }
 }
