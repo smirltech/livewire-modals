5ea09ea517fcbaa3e7a9f67f5bdb34fd06bbc67d
Marien Mupenda <marien.mupenda@gmail.com>
2023-05-22 18:11:38 +0200

    update delete model

--- resources/views/livewire/delete-model.blade.php
+++ resources/views/livewire/delete-model.blade.php
@@ -1,8 +1,9 @@
 <x-modals::base theme="danger">
-    <x-slot name="title">Suppression {{$label}}</x-slot>
+    <x-slot name="title">Suppression de {{$label}}</x-slot>
     <div class="row">
         <div class="col-md-12">
-            <p>Êtes-vous sûr de vouloir supprimer la donnée <strong class="badge badge-danger">{{$label}}</strong> ?<br>De
+            <p>Êtes-vous sûr de vouloir supprimer le {{class_basename($model)}} <strong
+                        class="badge badge-danger">{{$label}}</strong> ?<br>De
                 plus, cette action est
                 irréversible.</p>
         </div>
