d1debd76290b783915152e620cf52142d7241778
Marien Mupenda <marien.mupenda@gmail.com>
2024-01-11 22:09:26 +0200

    add size to modal

--- resources/views/components/base.blade.php
+++ resources/views/components/base.blade.php
@@ -1,8 +1,8 @@
-@props(['title' => null, 'footer' => null,'theme'=>'primary','header'=>null])
+@props(['title' => null, 'footer' => null,'theme'=>'primary','header'=>null,'size'=>'md'])
 @php
     $textColor = in_array($theme,['primary','secondary','success','danger','warning','info','light','dark'])?'text-white':'text-dark';
 @endphp
-<div class="modal-dialog">
+<div class="modal-dialog modal-{{$size}}">
     <div class="modal-content">
         @if($title or $header)
             <div class="modal-header bg-{{$theme}}">
